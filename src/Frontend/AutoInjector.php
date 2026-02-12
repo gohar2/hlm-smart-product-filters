70adf3d3565d08e276d590f659cc04c84963cf88
Gohar <gohar.freelancer@gmail.com>
2026-02-12 23:23:50 +0500

    Fixed issue where it was auto injected on the hook even when config was disabled for all pages

--- src/Frontend/AutoInjector.php
+++ src/Frontend/AutoInjector.php
@@ -42,13 +42,13 @@ final class AutoInjector
             return;
         }
 
-        if (is_shop() && empty($config['global']['auto_on_shop'])) {
-            return;
-        }
-        if (is_product_category() && empty($config['global']['auto_on_categories'])) {
-            return;
-        }
-        if (is_product_tag() && empty($config['global']['auto_on_tags'])) {
+        $should_render =
+        (is_shop() && !empty($config['global']['auto_on_shop'])) ||
+        (is_product_category() && !empty($config['global']['auto_on_categories'])) ||
+        (is_product_tag() && !empty($config['global']['auto_on_tags'])) ||
+        (is_tax() && !is_product_category() && !is_product_tag());
+
+        if (!$should_render) {
             return;
         }
 
